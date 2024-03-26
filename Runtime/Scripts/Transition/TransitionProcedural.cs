eace4c36d4ae242eb9db1ccb0c15a357deee7dbe
MrSliddes <59771866+MrSliddes@users.noreply.github.com>
2024-03-26 20:22:09 +0100

    v1.13.18

--- Runtime/Scripts/Transition/TransitionProcedural.cs
+++ Runtime/Scripts/Transition/TransitionProcedural.cs
@@ -25,6 +25,7 @@ namespace SLIDDES.UI
         [SerializeField] private Transform siblingTransform;
 
         [Header("OnEnable")]
+        [SerializeField] private bool transformOnEnable = true;
         [SerializeField] private float timeOnEnable = 0.25f;
         [SerializeField] private bool onEnableDelayBySiblingIndex;
         [SerializeField] private Vector3 onEnableLocalPosition;
@@ -32,28 +33,33 @@ namespace SLIDDES.UI
         [SerializeField] private Vector3 onEnableLocalScale = Vector3.one;
 
         [Header("Select")]
+        [SerializeField] private bool transformOnSelect = true;
         [SerializeField] private float timeToSelect = 0.1f;
         [SerializeField] private Vector3 selectLocalPosition;
         [SerializeField] private Vector3 selectLocalEulerRotation;
         [SerializeField] private Vector3 selectLocalScale = new Vector3(1.1f, 1.1f, 1.1f);
 
         [Header("Hovering")]
+        [SerializeField] private bool transformOnHover = true;
         [SerializeField] private float transitionToHoverTime = 0.1f;
         [SerializeField] private AnimationCurve hoveringLocalScaleAnimationCurve = new AnimationCurve(new Keyframe[] { new Keyframe(0, 1.1f), new Keyframe(0.5f, 1f), new Keyframe(1, 1.1f) });
 
         [Header("Deselect")]
+        [SerializeField] private bool transformOnDeselect = true;
         [SerializeField] private float timeToDeselect = 0.1f;
         [SerializeField] private Vector3 deselectLocalPosition;
         [SerializeField] private Vector3 deselectLocalEulerRotation;
         [SerializeField] private Vector3 deselectLocalScale = new Vector3(1, 1, 1);
 
         [Header("Pointer Down")]
+        [SerializeField] private bool transformOnPointerDown = true;
         [SerializeField] private float timePointerDown = 0.1f;
         [SerializeField] private Vector3 pointerDownLocalPosition;
         [SerializeField] private Vector3 pointerDownLocalEulerRotation;
         [SerializeField] private Vector3 pointerDownLocalScale = new Vector3(0.8f, 0.8f, 0.8f);
 
         [Header("Pointer Up")]
+        [SerializeField] private bool transformOnPointerUp = true;
         [SerializeField] private float timePointerUp = 0.2f;
         [SerializeField] private Vector3 pointerUpLocalPosition;
         [SerializeField] private Vector3 pointerUpLocalEulerRotation;
@@ -78,21 +84,24 @@ namespace SLIDDES.UI
                 yield return new WaitForSecondsRealtime(timeOnEnable * index);
             }
 
-            while(transitionTimer < timeOnEnable)
+            if(transformOnEnable)
             {
-                float t = TransitionEasing.EaseOutCubic(transitionTimer / timeOnEnable);
-                targetGraphic.transform.localPosition = Vector3.Lerp(onEnableLocalPosition, deselectLocalPosition, t);
-                targetGraphic.transform.localRotation = Quaternion.Euler(Vector3.Lerp(onEnableLocalEulerRotation, deselectLocalEulerRotation, t));
-                targetGraphic.transform.localScale = Vector3.Lerp(onEnableLocalScale, deselectLocalScale, t);
-                transitionTimer += Time.unscaledDeltaTime;
-                yield return null;
-            }
+                while(transitionTimer < timeOnEnable)
+                {
+                    float t = TransitionEasing.EaseOutCubic(transitionTimer / timeOnEnable);
+                    targetGraphic.transform.localPosition = Vector3.Lerp(onEnableLocalPosition, deselectLocalPosition, t);
+                    targetGraphic.transform.localRotation = Quaternion.Euler(Vector3.Lerp(onEnableLocalEulerRotation, deselectLocalEulerRotation, t));
+                    targetGraphic.transform.localScale = Vector3.Lerp(onEnableLocalScale, deselectLocalScale, t);
+                    transitionTimer += Time.unscaledDeltaTime;
+                    yield return null;
+                }
 
-            // Done
-            // Deadset
-            targetGraphic.transform.localPosition = deselectLocalPosition;
-            targetGraphic.transform.localRotation = Quaternion.Euler(deselectLocalEulerRotation);
-            targetGraphic.transform.localScale = deselectLocalScale;
+                // Done
+                // Deadset
+                targetGraphic.transform.localPosition = deselectLocalPosition;
+                targetGraphic.transform.localRotation = Quaternion.Euler(deselectLocalEulerRotation);
+                targetGraphic.transform.localScale = deselectLocalScale;
+            }
 
             transitionTimer = 0;
             yield break;
@@ -100,21 +109,24 @@ namespace SLIDDES.UI
 
         protected override IEnumerator EnterAsync()
         {
-            while(transitionTimer < timeToSelect)
+            if(transformOnSelect)
             {
-                float t = transitionTimer / timeToSelect;
-                targetGraphic.transform.localPosition = Vector3.Lerp(deselectLocalPosition, selectLocalPosition, t);
-                targetGraphic.transform.localRotation = Quaternion.Euler(Vector3.Lerp(deselectLocalEulerRotation, selectLocalEulerRotation, t));
-                targetGraphic.transform.localScale = Vector3.Lerp(deselectLocalScale, selectLocalScale, t);
-                transitionTimer += Time.unscaledDeltaTime;
-                yield return null;
-            }
+                while(transitionTimer < timeToSelect)
+                {
+                    float t = transitionTimer / timeToSelect;
+                    targetGraphic.transform.localPosition = Vector3.Lerp(deselectLocalPosition, selectLocalPosition, t);
+                    targetGraphic.transform.localRotation = Quaternion.Euler(Vector3.Lerp(deselectLocalEulerRotation, selectLocalEulerRotation, t));
+                    targetGraphic.transform.localScale = Vector3.Lerp(deselectLocalScale, selectLocalScale, t);
+                    transitionTimer += Time.unscaledDeltaTime;
+                    yield return null;
+                }
 
-            // Done
-            // Deadset
-            targetGraphic.transform.localPosition = selectLocalPosition;
-            targetGraphic.transform.localRotation = Quaternion.Euler(selectLocalEulerRotation);
-            targetGraphic.transform.localScale = selectLocalScale;
+                // Done
+                // Deadset
+                targetGraphic.transform.localPosition = selectLocalPosition;
+                targetGraphic.transform.localRotation = Quaternion.Euler(selectLocalEulerRotation);
+                targetGraphic.transform.localScale = selectLocalScale;
+            }
 
             transitionTimer = 0;
             Update();
@@ -126,23 +138,26 @@ namespace SLIDDES.UI
             transitionTimer = 0;
             interruptedLocalScale = targetGraphic.transform.localScale;
 
-            while(transitionTimer < transitionToHoverTime)
+            if(transformOnHover)
             {
-                yield return null;
-                float t = transitionTimer / transitionToHoverTime;
-                transitionTimer += Time.unscaledDeltaTime;
-                targetGraphic.transform.localScale = Vector3.Lerp(interruptedLocalScale, hoveringLocalScaleAnimationCurve.Evaluate(0) * Vector3.one, t);
-            }
-            transitionTimer = 0;
-
-            while(true)
-            {
-                yield return null;
-                targetGraphic.transform.localScale = Vector3.one * hoveringLocalScaleAnimationCurve.Evaluate(transitionTimer);
-                transitionTimer += Time.unscaledDeltaTime;
-                if(transitionTimer > hoveringLocalScaleAnimationCurve[hoveringLocalScaleAnimationCurve.length - 1].time)
+                while(transitionTimer < transitionToHoverTime)
                 {
-                    transitionTimer = 0;
+                    yield return null;
+                    float t = transitionTimer / transitionToHoverTime;
+                    transitionTimer += Time.unscaledDeltaTime;
+                    targetGraphic.transform.localScale = Vector3.Lerp(interruptedLocalScale, hoveringLocalScaleAnimationCurve.Evaluate(0) * Vector3.one, t);
+                }
+                transitionTimer = 0;
+
+                while(true)
+                {
+                    yield return null;
+                    targetGraphic.transform.localScale = Vector3.one * hoveringLocalScaleAnimationCurve.Evaluate(transitionTimer);
+                    transitionTimer += Time.unscaledDeltaTime;
+                    if(transitionTimer > hoveringLocalScaleAnimationCurve[hoveringLocalScaleAnimationCurve.length - 1].time)
+                    {
+                        transitionTimer = 0;
+                    }
                 }
             }
         }
@@ -153,21 +168,24 @@ namespace SLIDDES.UI
             interruptedLocalEulerRotation = targetGraphic.transform.localEulerAngles;
             interruptedLocalScale = targetGraphic.transform.localScale;
 
-            while(transitionTimer < timeToDeselect)
+            if(transformOnDeselect)
             {
-                float t = transitionTimer / timeToDeselect;
-                targetGraphic.transform.localPosition = Vector3.Lerp(interruptedLocalPosition, deselectLocalPosition, t);
-                targetGraphic.transform.localRotation = Quaternion.Euler(Vector3.Lerp(interruptedLocalEulerRotation, deselectLocalEulerRotation, t));
-                targetGraphic.transform.localScale = Vector3.Lerp(interruptedLocalScale, deselectLocalScale, t);
-                transitionTimer += Time.unscaledDeltaTime;
-                yield return null;
-            }
+                while(transitionTimer < timeToDeselect)
+                {
+                    float t = transitionTimer / timeToDeselect;
+                    targetGraphic.transform.localPosition = Vector3.Lerp(interruptedLocalPosition, deselectLocalPosition, t);
+                    targetGraphic.transform.localRotation = Quaternion.Euler(Vector3.Lerp(interruptedLocalEulerRotation, deselectLocalEulerRotation, t));
+                    targetGraphic.transform.localScale = Vector3.Lerp(interruptedLocalScale, deselectLocalScale, t);
+                    transitionTimer += Time.unscaledDeltaTime;
+                    yield return null;
+                }
 
-            // Done
-            // Deadset
-            targetGraphic.transform.localPosition = deselectLocalPosition;
-            targetGraphic.transform.localRotation = Quaternion.Euler(deselectLocalEulerRotation);
-            targetGraphic.transform.localScale = deselectLocalScale;
+                // Done
+                // Deadset
+                targetGraphic.transform.localPosition = deselectLocalPosition;
+                targetGraphic.transform.localRotation = Quaternion.Euler(deselectLocalEulerRotation);
+                targetGraphic.transform.localScale = deselectLocalScale;
+            }
 
             transitionTimer = 0;
             yield break;
@@ -184,21 +202,24 @@ namespace SLIDDES.UI
 
         protected override IEnumerator PointerDownAsync()
         {
-            while(transitionTimer < timePointerDown)
+            if(transformOnPointerDown)
             {
-                float t = transitionTimer / timePointerDown;
-                targetGraphic.transform.localPosition = Vector3.Lerp(interruptedLocalPosition, pointerDownLocalPosition, t);
-                targetGraphic.transform.localRotation = Quaternion.Euler(Vector3.Lerp(interruptedLocalEulerRotation, pointerDownLocalEulerRotation, t));
-                targetGraphic.transform.localScale = Vector3.Lerp(interruptedLocalScale, pointerDownLocalScale, t);
-                transitionTimer += Time.unscaledDeltaTime;
-                yield return null;
-            }
+                while(transitionTimer < timePointerDown)
+                {
+                    float t = transitionTimer / timePointerDown;
+                    targetGraphic.transform.localPosition = Vector3.Lerp(interruptedLocalPosition, pointerDownLocalPosition, t);
+                    targetGraphic.transform.localRotation = Quaternion.Euler(Vector3.Lerp(interruptedLocalEulerRotation, pointerDownLocalEulerRotation, t));
+                    targetGraphic.transform.localScale = Vector3.Lerp(interruptedLocalScale, pointerDownLocalScale, t);
+                    transitionTimer += Time.unscaledDeltaTime;
+                    yield return null;
+                }
 
-            // Done
-            // Deadset
-            targetGraphic.transform.localPosition = pointerDownLocalPosition;
-            targetGraphic.transform.localRotation = Quaternion.Euler(pointerDownLocalEulerRotation);
-            targetGraphic.transform.localScale = pointerDownLocalScale;
+                // Done
+                // Deadset
+                targetGraphic.transform.localPosition = pointerDownLocalPosition;
+                targetGraphic.transform.localRotation = Quaternion.Euler(pointerDownLocalEulerRotation);
+                targetGraphic.transform.localScale = pointerDownLocalScale;
+            }
 
             transitionTimer = 0;
             yield break;
@@ -215,25 +236,49 @@ namespace SLIDDES.UI
 
         protected override IEnumerator PointerUpAsync()
         {
-            while(transitionTimer < timePointerUp)
+            if(transformOnPointerUp)
             {
-                float t = transitionTimer / timePointerUp;
-                targetGraphic.transform.localPosition = Vector3.Lerp(interruptedLocalPosition, pointerUpLocalPosition, t);
-                targetGraphic.transform.localRotation = Quaternion.Euler(Vector3.Lerp(interruptedLocalEulerRotation, pointerUpLocalEulerRotation, t));
-                targetGraphic.transform.localScale = Vector3.Lerp(interruptedLocalScale, pointerUpLocalScale, t) * pointerUpLocalScaleAnimationCurve.Evaluate(t);
-                transitionTimer += Time.unscaledDeltaTime;
-                yield return null;
-            }
+                while(transitionTimer < timePointerUp)
+                {
+                    float t = transitionTimer / timePointerUp;
+                    targetGraphic.transform.localPosition = Vector3.Lerp(interruptedLocalPosition, pointerUpLocalPosition, t);
+                    targetGraphic.transform.localRotation = Quaternion.Euler(Vector3.Lerp(interruptedLocalEulerRotation, pointerUpLocalEulerRotation, t));
+                    targetGraphic.transform.localScale = Vector3.Lerp(interruptedLocalScale, pointerUpLocalScale, t) * pointerUpLocalScaleAnimationCurve.Evaluate(t);
+                    transitionTimer += Time.unscaledDeltaTime;
+                    yield return null;
+                }
 
-            // Done
-            // Deadset
-            targetGraphic.transform.localPosition = pointerUpLocalPosition;
-            targetGraphic.transform.localRotation = Quaternion.Euler(pointerUpLocalEulerRotation);
-            targetGraphic.transform.localScale = pointerUpLocalScale;
+                // Done
+                // Deadset
+                targetGraphic.transform.localPosition = pointerUpLocalPosition;
+                targetGraphic.transform.localRotation = Quaternion.Euler(pointerUpLocalEulerRotation);
+                targetGraphic.transform.localScale = pointerUpLocalScale;
+            }
 
             transitionTimer = 0;
             Update();
             yield break;
         }
     }
+
+    //[System.Serializable]
+    //public class TransformBlock
+    //{
+    //    public bool usePosition;
+    //    public bool positionIsLocal = true;
+    //    public Vector3 position;
+    //    public bool useEulerRotation;
+    //    public bool eulerRotationIsLocal = true;
+    //    public Vector3 eulerRotation;
+    //    public bool useScale;
+    //    public bool scaleIsLocal = true;
+    //    public Vector3 scale;
+
+    //    public TransformBlock(Vector3 position, Vector3 eulerRotation, Vector3 scale)
+    //    {
+    //        this.position = position;
+    //        this.eulerRotation = eulerRotation;
+    //        this.scale = scale;
+    //    }
+    //}
 }
